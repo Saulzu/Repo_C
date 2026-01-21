42248b05573feb992998ef8ee457cf643890a483
Saulzu <124089079+Saulzu@users.noreply.github.com>
2026-01-21 02:41:40 +0000

    first_steps

---
+++ main.c
@@ -0,0 +1,10 @@
+
+int main()
+{
+    int precio = 10;
+    int cuenta = 0;
+
+    cuenta = precio + 5;
+    printf("El valor de cuenta es %d", cuenta);
+    return 0;
+}
---
+++ par.c
@@ -0,0 +1,8 @@
+int main()
+{
+float graC = 17;
+float graF = 0;
+graF = 9.0/5.0 * graC + 32; 
+printf("Los %f grados celsius aon %f Fahrenheit ", graC, graF);
+return 0;
+}
