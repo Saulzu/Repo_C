a36d709f1361afe8b2791ff523a5305f179443a9
Saulzu <124089079+Saulzu@users.noreply.github.com>
2026-01-26 18:35:32 +0000

    uber

---
+++ uber.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+int main(){
+    float x1 = 2.5;
+    float y1 = 4.5;
+    float x2 = 5.2;
+    float y2 = 1.5;
+    float xp, yp, d1, d2;
+
+    printf("Ingresa la coordenada X del pasajero:");
+    scanf("%f", &xp)
+    printf("Ingresa la coordenada Y del pasajero");
+    scanf("%f", &yp)
+    
+    
+}
