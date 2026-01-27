1df85bd750ece48fa8f265cf0f5eb3edbcac7c75
Saulzu <124089079+Saulzu@users.noreply.github.com>
2026-01-27 01:46:26 +0000

    clean

---
+++ peso.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+   #include <stdio.h>
+   #include <math.h>
+
+   int main() 
+   {
+        float Peso, Altura;
+        printf("Introduce tu Peso:");
+        scanf("%f", &Peso);
+        printf("Introduce tu Altura:");
+    scanf("%f", &Altura);
+    float ideal = 0.0;
+    float IMC = Peso / ((Altura)*(Altura));
+    if (IMC<25) {
+        ideal = 25 - IMC ;
+        printf("Debe subir %f kg", ideal);
+    }
+    else {
+        ideal = IMC - 25 ;
+        printf("Debes bajar %f kg", ideal);
+    }
+    return 0;
+    
+}
--- uber.c
+++ uber.c
@@ -1,26 +1,32 @@
-#include <studio.h>
+#include <stdio.h>
+#include <math.h>
 
-int main(){
+int main() 
+{
     float x1 = 2.5;
     float y1 = 4.5;
     float x2 = 5.2;
     float y2 = 1.5;
-    float xp, yp, d1, d2;
+    float xp;
+    float yp;
+    float d1 = 0;
+    float d2 = 0;
 
     printf("Ingresa la coordenada X del pasajero:");
-    scanf("%f", &xp)
+    scanf("%f", &xp);
     printf("Ingresa la coordenada Y del pasajero");
-    scanf("%f", &yp)
+    scanf("%f", &yp);
     
-    d1 = sqrt(((x2-xp)*(x2-xp))+((y2-yp)*(y2-yp)));
+    d1 = sqrt(((x1-xp)*(x1-xp))+((y1-yp)*(y1-yp)));
     d2 = sqrt(((x2-xp)*(x2-xp))+((y2-yp)*(y2-yp)));
 
-    print("La distacia al primer uber es: %f", d1);
-    print("La distacia al segundo uber es: %f", d2);
+    printf("La distacia al primer uber es: %f", d1);
+    printf("La distacia al segundo uber es: %f", d2);
 
     if(d1<d2){
-        print("El pasajero esta mas cerca del Uber 1");
+        printf("El pasajero esta mas cerca del Uber 1");
     } else {
-        print("El pasajero esta mas cerca del Uber 2");
+        printf("El pasajero esta mas cerca del Uber 2");
     }
+    return 0;
 }
